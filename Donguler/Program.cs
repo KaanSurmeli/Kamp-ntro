3b670b3c7dc4906be6e0b80059720dde8315e11f
Kaan Sürmeli <Kaan Srmeli@DESKTOP-36EIR06>
2023-02-19 16:45:23 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+string kurs1 = "Yazılım Geliştiricisi Yetiştirme Kampı";
+string kurs2 = "Programlamaya başlangıç için temel kurs";
+string kurs3 = "Java";
+//array dizi
+string[] kurslar = new string[] {"Yazılım Geliştiricisi yetiştirme kampı",
+"Programlamaya başlangıç için temel kurs","Java","Python"};
+
+for (int i = 0; i <kurslar.Length; i++)
+{
+    Console.WriteLine(kurslar[i]);
+}
+Console.WriteLine("Ara");
+foreach  (string kurs in kurslar)
+{
+    Console.WriteLine(kurs);
+}
---
+++ Kampİntro/Program.cs
@@ -0,0 +1,24 @@
+﻿string kategoriEtiketi = "kategoriler";
+int ogrenciSayisi = 32000;
+double faizOrani = 1.45;
+bool sistemeGirisYapmisMi = true;
+double dolarDun = 7.55;
+double dolarBugun = 7.45;
+
+
+if (dolarDun>dolarBugun)
+{
+    Console.WriteLine("Azalış Butonu");
+}
+else if (dolarDun < dolarBugun)
+{
+    Console.WriteLine("Artış Butonu");
+}
+else
+{
+    Console.WriteLine("Değişmedi Butonu");
+}
+if (sistemeGirisYapmisMi == true)
+{
+    Console.WriteLine("Kullanıcı ayarları Butonu");
+}
